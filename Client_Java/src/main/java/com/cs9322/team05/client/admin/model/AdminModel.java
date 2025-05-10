e1acb2e5820089dc0b1b934b131271ed3282703e
aaron miguel cardenas <2242828@slu.edu.ph>
2025-05-11 07:43:34 +0800

    AdminModel inbound

---
+++ Client_Java/src/main/java/com/cs9322/team05/client/admin/model/AdminModel.java
@@ -0,0 +1,25 @@
+package com.cs9322.team05.client.admin.model;
+
+import ModifiedHangman.*;
+import org.omg.CORBA.*;
+import org.omg.CosNaming.*;
+
+public class AdminModel {
+
+    private static AdminService adminServiceImpl;
+
+    public AdminModel() {
+        try {
+            ORB orb = ORB.init(args, null);
+
+            org.omg.CORBA.Object obj = orb.resolve_initial_references("NameService");
+
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(obj);
+
+            String name = "AdminService";
+            adminServiceImpl = AdminServiceHelper.narrow(ncRef.resolve_str(name));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
