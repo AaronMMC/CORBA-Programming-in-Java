8d963dfdf6b64db448915eae0852678be3ab588a
aaron miguel cardenas <2242828@slu.edu.ph>
2025-05-11 08:00:32 +0800

    AdminModel inbound

--- Client_Java/src/main/java/com/cs9322/team05/client/admin/model/AdminModel.java
+++ Client_Java/src/main/java/com/cs9322/team05/client/admin/model/AdminModel.java
@@ -21,4 +21,30 @@ public class AdminModel {
             e.printStackTrace();
         }
     }
+
+    public boolean create_player(String username, String password, String token) {
+        return adminServiceImpl.create_player(String username, String password, String token);
+    }
+
+    public boolean update_player(String username, String password, String token) {
+        return adminServiceImpl.update_player(String username, String new_password, String token);
+    }
+
+    public boolean delete_player(String username, String token) {
+        return adminServiceImpl.delete_player(String username, String token);
+    }
+
+    //TODO: Search Player method that has questionable return type. in the AdminServiceImpl.
+
+    public void set_waiting_time(int seconds, String token) {
+        adminServiceImpl.set_waiting_time(int seconds, String token);
+    }
+
+    public int get_waiting_time(String token) {
+        adminServiceImpl.get_waiting_time(String token);
+    }
+
+    public int get_round_duration(String token) {
+        adminServiceImpl.get_round_duration(String token);
+    }
 }
