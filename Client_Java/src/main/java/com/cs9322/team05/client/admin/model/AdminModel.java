734ce46fef99ba4681b6a642f23118bdd3765abd
aaron miguel cardenas <2242828@slu.edu.ph>
2025-05-15 17:46:43 +0800

    Adminfix

--- Client_Java/src/main/java/com/cs9322/team05/client/admin/model/AdminModel.java
+++ Client_Java/src/main/java/com/cs9322/team05/client/admin/model/AdminModel.java
@@ -1,5 +1,6 @@
     package com.cs9322.team05.client.admin.model;
 
+    import ModifiedHangman.AdminNotLoggedInException;
     import ModifiedHangman.AdminService;
     import ModifiedHangman.Player;
     import org.omg.CORBA.*;
@@ -15,19 +16,19 @@
             AdminModel.adminServiceImpl = adminService;
         }
 
-        public void create_player(String username, String password, String token) {
+        public void create_player(String username, String password, String token) throws AdminNotLoggedInException {
             adminServiceImpl.create_player(username,password,token);
         }
 
-        public void update_player(String username, String password, String token) {
+        public void update_player(String username, String password, String token) throws AdminNotLoggedInException {
             adminServiceImpl.update_player(username,password,token);
         }
 
-        public void delete_player(String username, String token) {
+        public void delete_player(String username, String token) throws AdminNotLoggedInException {
             adminServiceImpl.delete_player(username,token);
         }
 
-        public Player search_player (String keyword, String token) {
+        public Player search_player (String keyword, String token) throws AdminNotLoggedInException {
             return adminServiceImpl.search_player(keyword,token);
         }
 
@@ -35,19 +36,19 @@
             return adminServiceImpl.get_all_players(token);
         }
 
-        public void set_waiting_time(int seconds, String token) {
+        public void set_waiting_time(int seconds, String token) throws AdminNotLoggedInException {
             adminServiceImpl.set_waiting_time(seconds,token);
         }
 
-        public void set_round_duration(int seconds, String token) {
+        public void set_round_duration(int seconds, String token) throws AdminNotLoggedInException {
             adminServiceImpl.set_round_duration(seconds,token);
         }
 
-        public int get_waiting_time(String token) {
+        public int get_waiting_time(String token) throws AdminNotLoggedInException {
             return adminServiceImpl.get_waiting_time(token);
         }
 
-        public int get_round_duration(String token) {
+        public int get_round_duration(String token) throws AdminNotLoggedInException {
             return adminServiceImpl.get_round_duration(token);
         }
     }
