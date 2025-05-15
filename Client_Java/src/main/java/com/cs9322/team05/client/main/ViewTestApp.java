2bc583e8ad390880fde2f47bfa24bca64ce2f14b
NARCISO JAVIER <2241812@slu.edu.ph>
2025-05-15 11:01:07 +0800

    Server

--- Client_Java/src/main/java/com/cs9322/team05/client/main/ViewTestApp.java
+++ Client_Java/src/main/java/com/cs9322/team05/client/main/ViewTestApp.java
@@ -1,10 +1,10 @@
 package com.cs9322.team05.client.main;
 
+import com.cs9322.team05.client.admin.view.AdminView;
+import com.cs9322.team05.client.player.services.HomeView;
 import com.cs9322.team05.client.player.view.GameView;
 import com.cs9322.team05.client.player.view.LoginView;
 import com.cs9322.team05.client.player.view.MatchmakingView;
-import com.cs9322.team05.client.player.services.HomeView;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
@@ -12,32 +12,35 @@ import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
 
 public class ViewTestApp extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage stage) {
         TabPane tabs = new TabPane();
 
-        // 1) Login screen (buttons won't do anything here)
+
         LoginView loginView = new LoginView(null);
         tabs.getTabs().add(new Tab("Login", loginView.createLoginPane()));
 
-        // 2) Home screen (buttons won't do anything here)
+
         HomeView homeView = new HomeView("dummy-token", null);
         tabs.getTabs().add(new Tab("Home", homeView.getRoot()));
 
-        // 3) Matchmaking screen
+
         MatchmakingView mmView = new MatchmakingView();
         tabs.getTabs().add(new Tab("Matchmaking", mmView.getRootPane()));
 
-        // 4) Game screen
+
         GameView gameView = new GameView();
         tabs.getTabs().add(new Tab("Game", gameView.getRoot()));
+
+        AdminView adminView = new AdminView("asd",null);
+        tabs.getTabs().add(new Tab("Admin", adminView.getScene().getRoot()));
 
         stage.setScene(new Scene(tabs, 1000, 700));
         stage.setTitle("UI Preview");
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
