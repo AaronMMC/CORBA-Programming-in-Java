2db7555f7b7dbe0d9d9c7c4ee48bee14d5f2a396
JIM HENDRIX BAG-EO <2243493@slu.edu.ph>
2025-05-15 10:59:06 +0800

    Add the database

---
+++ Server_Java/db/hangman.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 15, 2025 at 02:58 AM
+-- Server version: 8.0.36
+-- PHP Version: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hangman`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `game_settings`
+--
+
+DROP TABLE IF EXISTS `game_settings`;
+CREATE TABLE IF NOT EXISTS `game_settings` (
+  `id` int NOT NULL,
+  `round_length` int NOT NULL,
+  `waiting_time_length` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player`
+--
+
+DROP TABLE IF EXISTS `player`;
+CREATE TABLE IF NOT EXISTS `player` (
+  `username` varchar(20) NOT NULL,
+  `hashed_password` varchar(100) NOT NULL,
+  `totalWins` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Server_Java/src/main/java/com/cs9322/team05/server/dao/DatabaseConnection.java
@@ -0,0 +1,22 @@
+package com.cs9322.team05.server.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/hangman";
+    private static final String USER = "jimhendrix";
+    private static final String PASSWORD = "jimhendrix24";
+    private static Connection connection;
+
+    private DatabaseConnection() {}
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return connection;
+    }
+}
+
